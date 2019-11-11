3f13ad8d0826b621b3ecf41772d7659cbe09c368
exuberant-pool-9484 <alisherfayzimatov@gmail.com>
2019-11-11 20:01:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ end
 
 def monopoly_with_third_tier
    monopoly_hash = {railroads: {pieces: 4,rent_in_dollars: {one_piece_owned: 25,two_pieces_owned:50,three_pieces_owned:100,four_pieces_owned:200}, 
-    names: {reading_railroad: {}, pennsylvania_railroad:{},player3:"David Bowie",player4:"Led Zeppelin" }}}
+    names: {reading_railroad: {}, pennsylvania_railroad:{},b_and_o_railroad:{},player4:"Led Zeppelin" }}}
 
 
 end
