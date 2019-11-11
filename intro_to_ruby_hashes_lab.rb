534eab347e7dad46239afd9d1d645b428d003f16
exuberant-pool-9484 <alisherfayzimatov@gmail.com>
2019-11-11 19:51:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-   monopoly_hash = {railroads: {pieces: 4,rent_in_dollars: 1}}
+   monopoly_hash = {railroads: {pieces: 4,rent_in_dollars: int}}
 
 
 end
